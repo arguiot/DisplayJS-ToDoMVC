31d1d0f27512a91a8875b5ca34b7dd37c0b23dd3
Arthur Guiot <arguiot@gmail.com>
2018-03-18 18:56:05 +0800

    Modernized code + support newer versions

--- js/app.js
+++ js/app.js
@@ -1,127 +1,127 @@
 // Defining DisplayJS
-var $ = new DisplayJS(window);
+const $ = new DisplayJS(window);
 
-var count = $.select(".todos").length;
-var App = {
-	complete: function() {
-		$.all($.select(".toggle"), function(el) {
+let count = $.s(".todos").length;
+const App = {
+	complete() {
+		$.all($.s(".toggle"), el => {
 			$.on(el, "click", function() {
-				$.toggleClass($.parent($.s(this),1), "completed");
+				$.toggleClass($.parent($.toNodeList(this),1), "completed");
 			});
 		})
 	},
-	destroy: function() {
-		$.all($.select(".destroy"), function(el) {
+	destroy() {
+		$.all($.s(".destroy"), el => {
 			$.on(el, "click", function() {
-				$.single(".todo-list").removeChild($.parent($.s(this),1)[0])
-				count = $.select(".todos").length;
+				$.single(".todo-list").removeChild($.parent($.toNodeList(this),1)[0])
+				count = $.s(".todos").length;
 			});
 		})
 	},
-	clear: function() {
-		$.on($.select(".clear-completed"), "click", function () {
-			$.all($.select(".completed"), function(el) {
+	clear() {
+		$.on($.s(".clear-completed"), "click", () => {
+			$.all($.s(".completed"), el => {
 				$.remove(el);
 			})
-			count = $.select(".todos").length;
+			count = $.s(".todos").length;
 		})
 	},
-	edit: function() {
-		$.all($.select(".view > label"), function(el) {
+	edit() {
+		$.all($.s(".view > label"), el => {
 			$.on(el, "dblclick", function() {
 				$.hide($.parent($.s(this)))
 				$.show([$.parent($.s(this),1)[0].children[1]])
 				$.parent($.s(this),1)[0].children[1].value = $.parent($.s(this))[0].children[1].innerHTML
 				$.on([$.parent($.s(this),1)[0].children[1]], "keypress", function(e) {
 					if (e.which == 13 || event.keyCode == 13) {
-				        $.text([this.parentNode.children[0].children[1]], this.value)
-				        $.hide([this])
-						$.show([this.parentNode.children[0]])
+				        $.text($.toNodeList(this.parentNode.children[0].children[1]), this.value)
+				        $.hide($.toNodeList(this))
+						$.show($.toNodeList(this.parentNode.children[0]))
 				    }
 				});
 			})
 		})
 	},
-	toggle_all: function() {
-		$.on($.select(".toggle-all"), "click", function() {
+	toggle_all() {
+		$.on($.s(".toggle-all"), "click", function() {
 			if (this.checked == true) {
-				$.all($.select(".toggle"), function(el) {
+				$.all($.s(".toggle"), el => {
 					el[0].checked = true
-					$.addClass([el[0].parentNode.parentNode], "completed");
+					$.addClass($.toNodeList(el[0].parentNode.parentNode), "completed");
 				})
 			} else {
-				$.all($.select(".toggle"), function(el) {
+				$.all($.s(".toggle"), el => {
 					el[0].checked = false
-					$.removeClass([el[0].parentNode.parentNode], "completed");
+					$.removeClass($.toNodeList(el[0].parentNode.parentNode), "completed");
 				})
 			}
 		})
 	},
-	init: function() {
+	init() {
 		if ($.single(".todo-list").hasChildNodes()) {
-			$.show($.select(".main"))
-			$.show($.select(".footer"))
+			$.show($.s(".main"))
+			$.show($.s(".footer"))
 		} else {
-			$.hide($.select(".main"))
-			$.hide($.select(".footer"))
+			$.hide($.s(".main"))
+			$.hide($.s(".footer"))
 		}
-		var routes = {
+		const routes = {
 			"/active": App.active,
 			"/completed": App.completed,
 			"/": App.all
-		}
-		var router = Router(routes);
+		};
+		const router = Router(routes);
 		router.init();
 	},
-	new: function() {
-		$.on($.select(".new-todo"), "keypress", function(e) {
+	new() {
+		$.on($.s(".new-todo"), "keypress", e => {
 			if (e.which == 13 || event.keyCode == 13) {
-		        $.append($.select(".todo-list"), '<li class="todos"><div class="view"><input class="toggle" type="checkbox"><label>' + $.single(".new-todo").value +'</label><button class="destroy"></button></div><input class="edit" value="Rule the web"></li>')
-		        $.valEmpty($.select(".new-todo"))
+		        $.append($.s(".todo-list"), `<li class="todos"><div class="view"><input class="toggle" type="checkbox"><label>${$.single(".new-todo").value}</label><button class="destroy"></button></div><input class="edit" value="Rule the web"></li>`)
+		        $.valEmpty($.s(".new-todo"))
 		        init()
 		    }
 		});
 	},
-	all: function() {
-		$.all($.select(".todos"), function(el) {
+	all() {
+		$.all($.s(".todos"), el => {
 			$.show(el)
 		})
-		$.all($.s(".filter"), function(el) {
+		$.all($.s(".filter"), el => {
 			$.removeClass(el, "selected");
 		})
-		$.addClass([$.s(".filter")[0]], "selected")
+		$.addClass($.toNodeList($.s(".filter")[0]), "selected")
 	},
-	completed: function() {
-		$.all($.select(".todos"), function(el) {
+	completed() {
+		$.all($.s(".todos"), el => {
 			if (!$.hasClass(el, "completed")) {
 				$.hide(el)
 			} else {
 				$.show(el)
 			}
 		})
-		$.all($.s(".filter"), function(el) {
+		$.all($.s(".filter"), el => {
 			$.removeClass(el, "selected");
 		})
-		$.addClass([$.s(".filter")[2]], "selected")
+		$.addClass($.toNodeList($.s(".filter")[2]), "selected")
 	},
-	active: function() {
-		$.all($.select(".todos"), function(el) {
+	active() {
+		$.all($.s(".todos"), el => {
 			if ($.hasClass(el, "completed")) {
 				$.hide(el)
 			} else {
 				$.show(el)
 			}
 		})
-		$.all($.s(".filter"), function(el) {
+		$.all($.s(".filter"), el => {
 			$.removeClass(el, "selected");
 		})
-		$.addClass([$.s(".filter")[1]], "selected")
+		$.addClass($.toNodeList($.s(".filter")[1]), "selected")
 	}
 	
 };
 function init () {
 	// recount the number of items.
-	count = $.select(".todos").length;
+	count = $.s(".todos").length;
 	// invoke all the functions.
 	App.complete();
 	App.destroy();
